eab9ce89e18541064cd1f1116975a162f21cffde
unknown <mkangesh29@gmail.com>
2021-04-17 17:39:57 +0530

    change description

---
+++ data.js
@@ -0,0 +1,92 @@
+let items = [
+    {
+        article: "माझे प्रेमळ दीर",
+        writer: "श्रीमती इंदुमती महाजन",
+    },
+    {
+        article: "सहज बोलणे, हित उपदेश",
+        writer: "श्री. पांडुरंग तुळशीराम सोनावणे",
+    },
+    {
+        article: "आबा",
+        writer: "B.Z. Bhadke",
+    },
+    {
+        article: "मना रे जनी मौन मुद्रा धरावी",
+        writer: "सौ मंगला उत्तमराव खैरनार",
+    },
+    {
+        article: "काका मला वाचवा",
+        writer: "सौ. लीलावती जिभाऊ पवार",
+    },
+    {
+        article: "दाजी",
+        writer: "सुनिल वडगे",
+    },
+    {
+        article: "अरे संस्कार संस्कार",
+        writer: "सौ नलिनी दिनकर अभोणकर",
+    },
+    {
+        article: "साखरवाडी विद्यालय",
+        writer: "अनिकेत बोन्द्रे",
+    },
+    {
+        article: "महाजन सर",
+        writer: "नजीर शेख",
+    },
+    {
+        article: "आबा मामा",
+        writer: "सौ. ज्योतिमाई",
+    },
+    {
+        article: "काका आणि पत्र",
+        writer: "श्री कैलास महाजन",
+    },
+    {
+        article: "शिदोरी",
+        writer: "भावेश महाजन",
+    },
+    {
+        article: "आबा बाबा",
+        writer: "श्री वैभव पवार",
+    },
+    {
+        article: "आमचे साखरवाडीचे दाजी",
+        writer: "श्री. विलास माधवराव वडगे",
+    },
+    {
+        article: "Narendra bhai",
+        writer: "Shri. Sharad Desai and Mrs. Rekha Desai",
+    },
+    {
+        article: "माझे पप्पा",
+        writer: "सौ. वैशाली जगताप",
+    },
+    {
+        article: "स्तिथप्रज्ञ",
+        writer: "सौ. लता महाजन",
+    },
+    {
+        article: "Dear Abu",
+        writer: "Yashashree",
+    },
+    {
+        article: "Papa",
+        writer: "Lopa",
+    },
+    {
+        article: "माझे प्रेमळ दीर",
+        writer: "श्रीमती इंदुमती महाजन",
+    },
+    {
+        article: "पप्पा",
+        writer: "राहुल",
+    },
+    {
+        article: "माझे प्रेमळ दीर",
+        writer: "श्रीमती इंदुमती महाजन",
+    },
+
+
+]
---
+++ script.js
@@ -0,0 +1,1 @@
+console.log(items[3].article);
